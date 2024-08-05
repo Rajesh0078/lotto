75b18ecf759ae1dfce5ec4f54598877ecc80e6dd
Rajesh <peketirajesh007@gmail.com>
2024-08-05 14:51:23 +0530

    gdg

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        document.getElementById('loading_screen').style.display = 'none';
+        document.getElementById('home-page').style.display = 'block';
+    }, 3000);
+});
